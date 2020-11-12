810d8ea8634832824f9ab6684596fd47c214751d
Wayne Rocha <dev.waynerocha@gmail.com>
2020-11-12 18:15:26 -0300

    +JS(incompleta)
    Foi adicionada a função JS para obter as datas e para mostrar o resultado.
    Falta apenas o calculo para encontrar a quantidade de dias

---
+++ js/contador.js
@@ -0,0 +1,18 @@
+function Pegardatas() {
+    let data_inicio = document.getElementById('calendario__inicio').value;
+    let data_fim = document.getElementById('calendario__fim').value;
+    if (data_inicio && data_fim != "") {
+        data_inicio = new Date(data_inicio);
+        data_fim = new Date(data_fim);
+        let dias = calculardias(data_inicio, data_fim);
+        mostrardata(dias);
+    }
+}
+function calculardias(di, df) {
+    let d = 0;
+    //???
+    return d;
+}
+function mostrardata(d) {
+    document.getElementById('dias').value = d + " Dias";
+}
